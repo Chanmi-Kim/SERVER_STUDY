b9e23f7cbaf8f66cf85bcfa8941926a59e5c0521
Chanmi Kim <chanmi_dev@naver.com>
2019-06-12 17:39:39 +0900

    ADD FILE [Address.java]
    1. java 파일 추가
    
    - Address.java

---
+++ JSPTest/src/com/test/jsp/Address.java
@@ -0,0 +1,45 @@
+package com.test.jsp;
+
+//자바 빈 규약
+public class Address {
+
+	private String name;
+	private String age;
+	private String gender;
+	private String tel;
+	private String address;
+	
+	public String getName2() {
+		return name;
+	}
+	public void setName2(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+	
+}
