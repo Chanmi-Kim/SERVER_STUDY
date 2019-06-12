be12d3ae9b357890a552c7a33bdf8a2599eefc12
Chanmi Kim <chanmi_dev@naver.com>
2019-06-12 17:39:56 +0900

    ADD FILE [Ex07.java]
    1. java 파일 추가
    
    - Ex07.java

---
+++ JSPTest/src/com/test/jsp/Ex07.java
@@ -0,0 +1,27 @@
+package com.test.jsp;
+
+import javax.servlet.http.Cookie;
+
+public class Ex07 {
+
+	public int sum(int a, int b) {
+		
+		Cookie cookie  = new Cookie("name", "value");
+		//cookie.set
+		
+		return a + b;
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
