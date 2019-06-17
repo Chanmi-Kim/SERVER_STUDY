564eee2ba61c041cee401bbd5a1292cc5e73f2c0
Chanmi Kim <chanmi_dev@naver.com>
2019-06-17 13:31:16 +0900

    ADD FILE [CategoryDTO.java]
    1. java 파일 추가
    
    - CategoryDTO.java

---
+++ JSPTest/src/com/test/code/CategoryDTO.java
@@ -0,0 +1,30 @@
+package com.test.code;
+
+public class CategoryDTO {
+	
+	private String seq;
+	private String name;
+	private String color;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+
+}
