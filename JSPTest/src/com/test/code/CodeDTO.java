7393e53d756c175602898b8b3f1beb51057776d2
Chanmi Kim <chanmi_dev@naver.com>
2019-06-17 13:30:57 +0900

    ADD FILE [CodeDTO.java]
    1. java 파일 추가
    
    - CodeDTO.java

---
+++ JSPTest/src/com/test/code/CodeDTO.java
@@ -0,0 +1,51 @@
+package com.test.code;
+
+public class CodeDTO {
+
+	private String seq;
+	private String subject;
+	private String description;
+	private String code;
+	private String category;
+	private String regdate;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+	
+}
