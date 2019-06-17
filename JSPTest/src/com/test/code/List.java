cb694a5f1ce9ff8f42f1ac5c903b718be18b0dda
Chanmi Kim <chanmi_dev@naver.com>
2019-06-17 17:37:47 +0900

    MODIFY FILE [List.java]
    1. java 파일 수정
    
    - List.java

--- JSPTest/src/com/test/code/List.java
+++ JSPTest/src/com/test/code/List.java
@@ -1,5 +1,46 @@
 package com.test.code;
 
-public class List {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class List extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//1. DB 작업 > DAO 위임(select)
+		//2. 결과 반환 + JSP 호출
+		
+		//1.
+		CodeDAO dao = new CodeDAO();
+		
+		ArrayList<CodeDTO> list = dao.list();
+		
+		//2.
+		req.setAttribute("list", list);		
+		
+		RequestDispatcher dispatcher 
+			= req.getRequestDispatcher("/WEB-INF/views/code/list.jsp");
+		dispatcher.forward(req, resp);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
