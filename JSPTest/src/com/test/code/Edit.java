e8c359fe9a352ac670d96868b2f642746d3b5090
Chanmi Kim <chanmi_dev@naver.com>
2019-06-18 16:38:56 +0900

    Modify File [Edit.java]
    1. java 파일 수정
    
    - Edit.java

--- JSPTest/src/com/test/code/Edit.java
+++ JSPTest/src/com/test/code/Edit.java
@@ -1,5 +1,52 @@
 package com.test.code;
 
-public class Edit {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Edit extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//edit.do?seq=10
+		//1. 데이터 가져오기(seq)
+		//2. DB 작업 > DAO 위임(select where)
+		//3. 결과 반환 + JSP 호출
+		
+		//1.
+		String seq = req.getParameter("seq");
+		
+		//2.
+		CodeDAO dao = new CodeDAO();
+		CodeDTO dto = dao.get(seq);
+		
+		ArrayList<CategoryDTO> clist = dao.clist(); //과목 목록 > 라디오 버튼들
+		
+		req.setAttribute("dto", dto);//수정할 내용
+		req.setAttribute("clist", clist);
+		
+		RequestDispatcher dispatcher 
+			= req.getRequestDispatcher("/WEB-INF/views/code/edit.jsp");
+		dispatcher.forward(req, resp);
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
