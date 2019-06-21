8d45d1910539c222321673ca3012a6db128dd38e
Chanmi Kim <chanmi_dev@naver.com>
2019-06-21 13:43:49 +0900

    Add File [AnimalDTO.java]
    1. java 파일 추가
    
    - AnimalDTO.java

---
+++ AjaxTest/src/com/test/ajax/AnimalDTO.java
@@ -0,0 +1,37 @@
+package com.test.ajax;
+
+public class AnimalDTO {
+
+	private String seq;
+	private String type;
+	private String data;
+	private String regdate;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	
+	
+	
+}
