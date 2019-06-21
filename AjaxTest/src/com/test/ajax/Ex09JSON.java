c473aaf8af2cd2a79a0c722b70ca427647c368e8
Chanmi Kim <chanmi_dev@naver.com>
2019-06-21 15:51:42 +0900

    Add File [Ex09JSON.java]
    1. java 파일 수정
    
    - Ex09JSON.java

---
+++ AjaxTest/src/com/test/ajax/Ex09JSON.java
@@ -0,0 +1,47 @@
+package com.test.ajax;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ex09json.do")
+public class Ex09JSON extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//Ex09JSON.java
+		//1.
+		String type = req.getParameter("type");
+		String data = req.getParameter("data");
+		
+		//2.
+		AnimalDTO dto = new AnimalDTO();
+		dto.setType(type);
+		dto.setData(data);
+		
+		AjaxDAO dao = new AjaxDAO();
+		
+		dao.add(dto);
+		
+		ArrayList<AnimalDTO> list = dao.data(dto);
+		
+		req.setAttribute("list", list);
+		
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/ex09json.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+}
+
+
+
+
+
