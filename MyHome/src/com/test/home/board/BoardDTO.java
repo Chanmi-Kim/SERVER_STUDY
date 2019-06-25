c001fc6b4c502ee7d65f5c204cb0423442e848b8
Chanmi Kim <chanmi_dev@naver.com>
2019-06-25 16:51:28 +0900

    Add File [MyHome > com.test.home.board > BoardDTO.java]
    1. java 파일 추가
    
    - BoardDTO.java

---
+++ MyHome/src/com/test/home/board/BoardDTO.java
@@ -0,0 +1,79 @@
+package com.test.home.board;
+
+public class BoardDTO {
+
+	private String seq;
+	private String id;
+	private String subject;
+	private String content;
+	private String regdate;
+	private int readcount;
+	private String userip;
+	private String tag;
+	
+	private String name; //글쓴이 이름
+	private int gap; //글쓴뒤 지난 시간(분)
+	
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public String getUserip() {
+		return userip;
+	}
+	public void setUserip(String userip) {
+		this.userip = userip;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getGap() {
+		return gap;
+	}
+	public void setGap(int gap) {
+		this.gap = gap;
+	}
+	
+}
