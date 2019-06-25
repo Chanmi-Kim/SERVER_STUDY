ec75bc12af411f37170b20d28bb84099dd32b4a1
Chanmi Kim <chanmi_dev@naver.com>
2019-06-25 16:31:14 +0900

    Add File [MyHome > com.test.home.board > Del.java]
    1. java 파일 추가
    
    - Del.java
    - 게시판페이지 게시글 삭제 기능입니다.
    - MyHome 프로젝트의 board 패키지에 속한 java 파일입니다.

---
+++ MyHome/src/com/test/home/board/Del.java
@@ -0,0 +1,38 @@
+package com.test.home.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/board/del.do")
+public class Del extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//Del.java
+		//del.do?seq=10
+		
+		String seq = req.getParameter("seq");
+		
+		req.setAttribute("seq", seq);		
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/del.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+}
+
+
+
+
+
+
+
+
+
