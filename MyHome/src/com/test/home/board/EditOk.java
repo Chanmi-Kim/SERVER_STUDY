05fa458987c81416700b98b5e6b4eaa9c9aa56bd
Chanmi Kim <chanmi_dev@naver.com>
2019-06-25 16:40:22 +0900

    Add File [MyHome > com.test.home.board > EditOk.java]
    1. java 파일 추가
    
    - EditOk.java
    - 게시판페이지 게시글 삭제 업무를 처리하는 기능입니다.
    - MyHome 프로젝트의 board 패키지에 속한 java 파일입니다.

---
+++ MyHome/src/com/test/home/board/EditOk.java
@@ -0,0 +1,62 @@
+package com.test.home.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/board/editok.do")
+public class EditOk extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		//EditOk.java
+		// == AddOk.java
+		//1. 데이터 가져오기
+		//2. DB 작업 > DAO 위임(update)
+		//3. 결과 반환 + JSP 호출
+		
+		//1.
+		req.setCharacterEncoding("UTF-8");
+		HttpSession session = req.getSession();
+
+		String subject = req.getParameter("subject");
+		String content = req.getParameter("content");
+		String tag = req.getParameter("tag");
+		String seq = req.getParameter("seq");
+		
+		//2.
+		BoardDTO dto = new BoardDTO();
+		
+		dto.setSubject(subject);
+		dto.setContent(content);
+		dto.setTag(tag);
+		dto.setSeq(seq);
+		
+		BoardDAO dao = new BoardDAO();
+		
+		int result = dao.edit(dto);
+		
+		req.setAttribute("result", result);
+		req.setAttribute("seq", seq);
+
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/editok.jsp");
+		dispatcher.forward(req, resp);
+	}
+
+}
+
+
+
+
+
+
+
+
