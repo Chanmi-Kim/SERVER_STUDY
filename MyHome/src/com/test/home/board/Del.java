3e118fe4edd0d3528b8c71b74635325fd83c25dd
Chanmi Kim <chanmi_dev@naver.com>
2019-06-26 17:25:41 +0900

    Modify File [MyHome > com.test.home.board > Del.java]
    1. java 파일 수정
    
    - Del.java
    - 게시판페이지 게시글 삭제 기능입니다.
    - MyHome 프로젝트의 board 패키지에 속한 java 파일입니다.

--- MyHome/src/com/test/home/board/Del.java
+++ MyHome/src/com/test/home/board/Del.java
@@ -9,16 +9,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.home.AuthCheck;
+
 @WebServlet("/board/del.do")
 public class Del extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//인증 사용자?
+		AuthCheck auth = new AuthCheck(req.getSession(), resp);
+		auth.allow();
+		
+		
+		
+		
 
 		//Del.java
 		//del.do?seq=10
 		
 		String seq = req.getParameter("seq");
+		
+		BoardDAO dao = new BoardDAO();
+		auth.allow2(dao.get(seq).getId());
 		
 		req.setAttribute("seq", seq);		
 
