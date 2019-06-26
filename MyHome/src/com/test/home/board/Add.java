a3923cf7215eeca89627b79ad22f8a0b2f0f340d
Chanmi Kim <chanmi_dev@naver.com>
2019-06-26 17:24:24 +0900

    Modify File [MyHome > com.test.home.board > Add.java]
    1. java 파일 수정
    
    - Add.java
    - 게시판페이지 게시글 추가 기능입니다.
    - MyHome 프로젝트의 board 패키지에 속한 java 파일입니다.

--- MyHome/src/com/test/home/board/Add.java
+++ MyHome/src/com/test/home/board/Add.java
@@ -9,14 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.home.AuthCheck;
+
 @WebServlet("/board/add.do")
 public class Add extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		//Add.java
 		
+		//인증 사용자?
+		AuthCheck auth = new AuthCheck(req.getSession(), resp);
+		auth.allow();
+
+		
+		//Add.java
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/board/add.jsp");
 		dispatcher.forward(req, resp);
