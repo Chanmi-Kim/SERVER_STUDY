77e318c701748158413049219bf6f1bde1560c8d
Chanmi Kim <chanmi_dev@naver.com>
2019-06-26 17:33:08 +0900

    Add File [MyHome > com.test.home.board > SearchDTO.java]
    1. java 파일 추가
    
    - SearchDTO.java

---
+++ MyHome/src/com/test/home/board/SearchDTO.java
@@ -0,0 +1,41 @@
+package com.test.home.board;
+
+public class SearchDTO {
+	
+	private String seq;
+	private String columnName;
+	private String word;
+	private String regdate;
+	private String id;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getColumnName() {
+		return columnName;
+	}
+	public void setColumnName(String columnName) {
+		this.columnName = columnName;
+	}
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+}
