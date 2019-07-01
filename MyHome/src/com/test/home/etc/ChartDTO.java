f8318d17c8c2dbdfc9623766806768d4f08b6793
Chanmi Kim <chanmi_dev@naver.com>
2019-07-02 08:55:19 +0900

    Add File [MyHome > com.test.home.board > HashDTO.java]
    1. java 파일 추가
    
    - HashDTO.java
    
    2. 차트 기능 추가

---
+++ MyHome/src/com/test/home/etc/ChartDTO.java
@@ -0,0 +1,30 @@
+package com.test.home.etc;
+
+public class ChartDTO {
+
+	private String mon;
+	private String cnt;
+	private String buseo;
+	
+	public String getMon() {
+		return mon;
+	}
+	public void setMon(String mon) {
+		this.mon = mon;
+	}
+	public String getCnt() {
+		return cnt;
+	}
+	public void setCnt(String cnt) {
+		this.cnt = cnt;
+	}
+	public String getBuseo() {
+		return buseo;
+	}
+	public void setBuseo(String buseo) {
+		this.buseo = buseo;
+	}
+	
+	
+	
+}
