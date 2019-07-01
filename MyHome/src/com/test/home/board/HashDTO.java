7e6219818bfff5c7ae8592c577711487e0155d67
Chanmi Kim <chanmi_dev@naver.com>
2019-07-02 08:46:34 +0900

    Add File [MyHome > com.test.home.board > HashDTO.java]
    1. java 파일 추가
    
    - HashDTO.java
    
    2. 해시태그 기능 추가

---
+++ MyHome/src/com/test/home/board/HashDTO.java
@@ -0,0 +1,30 @@
+package com.test.home.board;
+
+public class HashDTO {
+
+	private String seq;
+	private String pseq;
+	private String hseq;
+	
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getPseq() {
+		return pseq;
+	}
+	public void setPseq(String pseq) {
+		this.pseq = pseq;
+	}
+	public String getHseq() {
+		return hseq;
+	}
+	public void setHseq(String hseq) {
+		this.hseq = hseq;
+	}
+	
+	
+	
+}
