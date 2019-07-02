159e66ac3257c38a32a732f66edb53511638a8b2
Chanmi Kim <chanmi_dev@naver.com>
2019-07-02 17:26:37 +0900

    Modify File [MyHome > com.test.home.etc > EtcDAO.java]
    1. java 파일 수정
    
    - EtcDAO.java

--- MyHome/src/com/test/home/etc/EtcDAO.java
+++ MyHome/src/com/test/home/etc/EtcDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import com.test.home.DBUtil;
+import com.test.home.member.MemberDTO;
 
 public class EtcDAO {
 
@@ -95,6 +96,120 @@ public class EtcDAO {
 		} catch (Exception e) {
 			
 			System.out.println("EtcDAO.listAll : " + e.toString());
+		}
+		
+		return null;
+	}
+
+	public ArrayList<ChartDTO1> list1() {
+		
+		try {
+			
+			String sql = "select (select name from tblMember where id = tblBoard.id) as name, count(*) as cnt from tblBoard group by id";
+			stat = conn.prepareStatement(sql);
+			rs = stat.executeQuery();
+			
+			ArrayList<ChartDTO1> list1 = new ArrayList<ChartDTO1>();
+			
+			while (rs.next()) {
+				ChartDTO1 dto = new ChartDTO1();
+				dto.setName(rs.getString("name"));
+				dto.setCnt(rs.getInt("cnt"));
+				list1.add(dto);
+			}
+			
+			return list1;
+			
+		} catch (Exception e) {
+			
+			System.out.println("EtcDAO.list1 : " + e.toString());
+		}
+		
+		return null;
+	}
+
+	public ArrayList<MemberDTO> mlist() {
+		
+		try {
+			
+			String sql = "select id, name from tblMember order by name desc";
+			stat = conn.prepareStatement(sql);
+			rs = stat.executeQuery();
+			
+			ArrayList<MemberDTO> mlist = new ArrayList<MemberDTO>();
+			
+			while (rs.next()) {
+				MemberDTO dto = new MemberDTO();
+				dto.setId(rs.getString("id"));
+				dto.setName(rs.getString("name"));
+				mlist.add(dto);
+			}
+			
+			return mlist;
+			
+		} catch (Exception e) {
+			
+			System.out.println("EtcDAO.mlist : " + e.toString());
+		}
+		
+		return null;
+	}
+
+	public ArrayList<ChartDTO2> list2() {
+		
+		try {
+			
+			String sql = "select " + 
+					"    (select count(*) from tblBoard where id=tblMember.id) as cnt1," + 
+					"    (select count(*) from tblComment where id=tblMember.id) as cnt2" + 
+					" from tblMember " + 
+					"    where id=(select id from (select id from tblMember order by name desc) " + 
+					"        where rownum=1)";
+			stat = conn.prepareStatement(sql);
+			rs = stat.executeQuery();
+			
+			ArrayList<ChartDTO2> list2 = new ArrayList<ChartDTO2>();
+			
+			while (rs.next()) {
+				ChartDTO2 dto = new ChartDTO2();
+				dto.setCnt1(rs.getInt("cnt1"));
+				dto.setCnt2(rs.getInt("cnt2"));
+				list2.add(dto);
+			}
+			
+			return list2;
+			
+		} catch (Exception e) {
+			
+			System.out.println("EtcDAO.list2 : " + e.toString());
+		}
+		
+		return null;
+	}
+
+	public ChartDTO2 get(String id) {
+		
+		try {
+			
+			String sql = "select  (select count(*) from tblBoard where id=tblMember.id) as cnt1,  (select count(*) from tblComment where id=tblMember.id) as cnt2  from tblMember where id=?";
+			stat = conn.prepareStatement(sql);
+			stat.setString(1, id);
+			
+			rs = stat.executeQuery();
+			
+			ChartDTO2 dto = new ChartDTO2();
+			
+			if (rs.next()) {
+				dto.setCnt1(rs.getInt("cnt1"));
+				dto.setCnt2(rs.getInt("cnt2"));
+			}
+			
+			return dto;
+			
+			
+		} catch (Exception e) {
+			
+			System.out.println("EtcDAO.get : " + e.toString());
 		}
 		
 		return null;
