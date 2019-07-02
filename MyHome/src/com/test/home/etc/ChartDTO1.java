7962cf2b2e65de1a67b2d8e2aa37e0865b71058d
Chanmi Kim <chanmi_dev@naver.com>
2019-07-02 17:30:57 +0900

    Add File [MyHome > com.test.home.etc > ChartDTO1.java]
    1. java 파일 추가
    
    - ChartDTO1.java

---
+++ MyHome/src/com/test/home/etc/ChartDTO1.java
@@ -0,0 +1,23 @@
+package com.test.home.etc;
+
+public class ChartDTO1 {
+	
+	private String name;
+	private int cnt;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCnt() {
+		return cnt;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	
+
+}
