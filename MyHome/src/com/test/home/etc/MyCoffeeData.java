3004fd59f3d0f036a46608a12914e9a46f68a798
Chanmi Kim <chanmi_dev@naver.com>
2019-07-08 23:21:38 +0900

    Modify File [MyHome > com.test.home.etc > MyCoffeeData.java]
    1. java 파일 수정
    
    - MyCoffeeData.java
    - MyHome 프로젝트의 etc 패키지에 속한 java 파일입니다.

--- MyHome/src/com/test/home/etc/MyCoffeeData.java
+++ MyHome/src/com/test/home/etc/MyCoffeeData.java
@@ -17,6 +17,71 @@ public class MyCoffeeData extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		//MyCoffeeData.java
+		String mode = req.getParameter("mode");
+		
+		if (mode == null || mode.equals("")) {
+			add(req, resp);
+		} else if (mode.equals("edit")) {
+			edit(req, resp);
+		} else if (mode.equals("del")) {
+			del(req, resp);
+		}
+		
+	}
+
+	private void edit(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+		String name = req.getParameter("name");
+		String seq = req.getParameter("seq");
+		
+		MyCoffeeDTO dto = new MyCoffeeDTO();
+		dto.setName(name);
+		dto.setSeq(seq);
+		
+		EtcDAO dao = new EtcDAO();
+		int result = dao.editCoffee(dto);
+		
+		resp.setContentType("application/json");//MIME
+		
+		PrintWriter writer = resp.getWriter();
+		
+		//{
+		//		"result": "1"
+		//}
+		writer.print("{");
+		writer.printf("\"result\":\"%d\"", result);
+		writer.print("}");
+		
+		writer.close();
+		
+	}
+
+	private void del(HttpServletRequest req, HttpServletResponse resp)  throws IOException {
+		
+		String seq = req.getParameter("seq");
+		
+		MyCoffeeDTO dto = new MyCoffeeDTO();
+		dto.setSeq(seq);
+		
+		EtcDAO dao = new EtcDAO();
+		int result = dao.delCoffee(dto);
+		
+		resp.setContentType("application/json");//MIME
+		
+		PrintWriter writer = resp.getWriter();
+		
+		//{
+		//		"result": "1"
+		//}
+		writer.print("{");
+		writer.printf("\"result\":\"%d\"", result);
+		writer.print("}");
+		
+		writer.close();
+		
+	}
+
+	private void add(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		String name = req.getParameter("name");
 		String lat = req.getParameter("lat");
 		String lng = req.getParameter("lng");
@@ -41,7 +106,6 @@ public class MyCoffeeData extends HttpServlet {
 		writer.print("}");
 		
 		writer.close();
-		
 	}
 
 }
